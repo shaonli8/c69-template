923477b1515ec5ce8915e8bb7916c352e48993ac
shaonli8 <72174470+shaonli8@users.noreply.github.com>
2021-09-25 10:36:10 +0530

    Create getCameraPermissions.js

---
+++ getCameraPermissions.js
@@ -0,0 +1,12 @@
+getCameraPermissions = async () =>{
+      const {status} = await Permissions.askAsync(Permissions.CAMERA);
+      
+      this.setState({
+        /*status === "granted" is true when user has granted permission
+          status === "granted" is false when user has not granted the permission
+        */
+        hasCameraPermissions: status === "granted",
+        buttonState: 'clicked',
+        scanned: false
+      });
+    }
