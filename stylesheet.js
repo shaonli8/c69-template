86004c5d3ed8c1c00e77663125f2242a150f2c62
shaonli8 <72174470+shaonli8@users.noreply.github.com>
2021-09-25 10:35:40 +0530

    Create stylesheet.js

---
+++ stylesheet.js
@@ -0,0 +1,19 @@
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    displayText:{
+      fontSize: 15,
+      textDecorationLine: 'underline'
+    },
+    scanButton:{
+      backgroundColor: '#2196F3',
+      padding: 10,
+      margin: 10
+    },
+    buttonText:{
+      fontSize: 20,
+    }
+  });
